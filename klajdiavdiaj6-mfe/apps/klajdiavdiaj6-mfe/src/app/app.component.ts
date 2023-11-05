cf350280a8f8a171fee4aec205ffb39b235d80e3
Ken Mau <ken_mau@hotmail.com>
2023-11-05 15:59:17 -0500

    klajdiavdiaj6-mfe

---
+++ klajdiavdiaj6-mfe/apps/klajdiavdiaj6-mfe/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'klajdiavdiaj6-mfe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'klajdiavdiaj6-mfe';
+}
